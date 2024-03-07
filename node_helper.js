e19538687d0f1ae224273aef9e22563ddc019d22
bugsounet <github@bugsounet.fr>
2024-03-07 22:24:07 +0100

    eslint fix

--- node_helper.js
+++ node_helper.js
@@ -1,73 +1,74 @@
-/**********************************
-* node_helper for EXT-Screen v1.3 *
-* BuGsounet ©03/22                *
-**********************************/
+/*****************************
+* node_helper for EXT-Screen *
+* BuGsounet                  *
+*****************************/
 
-const NodeHelper = require('node_helper')
-const LibScreen = require("./components/screenLib.js")
-const LibCron = require("./components/cronJob.js")
-var log = (...args) => { /* do nothing */ }
+const NodeHelper = require("node_helper");
+const LibScreen = require("./components/screenLib.js");
+const LibCron = require("./components/cronJob.js");
+
+var log = (...args) => { /* do nothing */ };
 
 module.exports = NodeHelper.create({
-  start: function() {
-    this.lib = {}
+  start () {
+    this.lib = {};
   },
 
-  socketNotificationReceived: function (notification, payload) {
+  socketNotificationReceived (notification, payload) {
     switch(notification) {
       case "INIT":
-        this.config = payload
-        this.initialize()
-        break
+        this.config = payload;
+        this.initialize();
+        break;
       case "WAKEUP":
-        this.screen.wakeup()
-        break
+        this.screen.wakeup();
+        break;
       case "FORCE_END":
-        this.screen.forceEnd()
-        break
+        this.screen.forceEnd();
+        break;
       case "LOCK":
-        this.screen.lock()
-        break
+        this.screen.lock();
+        break;
       case "UNLOCK":
-        this.screen.unlock()
-        break
+        this.screen.unlock();
+        break;
       case "LOCK_FORCE_END":
-        this.screen.forceLockOFF()
-        break
+        this.screen.forceLockOFF();
+        break;
       case "LOCK_FORCE_WAKEUP":
-        this.screen.forceLockON()
-        break
+        this.screen.forceLockON();
+        break;
     }
   },
 
-  initialize: function () {
-    if (this.config.debug) log = (...args) => { console.log("[SCREEN]", ...args) }
-    console.log("[SCREEN] EXT-Screen Version:", require('./package.json').version, "rev:", require('./package.json').rev)
+  initialize () {
+    if (this.config.debug) log = (...args) => { console.log("[SCREEN]", ...args); };
+    console.log("[SCREEN] EXT-Screen Version:", require("./package.json").version, "rev:", require("./package.json").rev);
     var callbacks= {
       /** LibCron **/
-      "cronState": (param) => {
-        this.screen.cronState(param)
+      cronState: (param) => {
+        this.screen.cronState(param);
       },
       /** LibScreen **/
-      "sendSocketNotification": (noti, params) => {
-        this.sendSocketNotification(noti, params)
+      sendSocketNotification: (noti, params) => {
+        this.sendSocketNotification(noti, params);
         //log("Callback Notification:", noti,params)
       },
-      "detector": (noti, param) => {
-        log("Callback Detector:", noti)
-        this.sendSocketNotification(noti)
+      detector: (noti, param) => {
+        log("Callback Detector:", noti);
+        this.sendSocketNotification(noti);
       },
-      "governor": (noti, param) => {
-        log("Callback Governor:", noti)
-        this.sendSocketNotification(noti)
-      },
-    }
+      governor: (noti, param) => {
+        log("Callback Governor:", noti);
+        this.sendSocketNotification(noti);
+      }
+    };
 
-    this.screen = new LibScreen(this.config, callbacks)
-    this.cron = new LibCron(this.config, callbacks)
+    this.screen = new LibScreen(this.config, callbacks);
+    this.cron = new LibCron(this.config, callbacks);
 
-    this.screen.activate()
-    this.cron.start()
-    this.sendSocketNotification("INITIALIZED")
+    this.screen.activate();
+    this.cron.start();
+    this.sendSocketNotification("INITIALIZED");
   }
 });
