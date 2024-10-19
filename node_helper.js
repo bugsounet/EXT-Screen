f9c1f38d82070c6c58fb624cd67bce778693dde2
bugsounet <github@bugsounet.fr>
2024-10-19 13:52:03 +0200

    cleaning node_helper

--- node_helper.js
+++ node_helper.js
@@ -4,20 +4,17 @@
 *****************************/
 
 const NodeHelper = require("node_helper");
-var log = (...args) => { /* do nothing */ };
 
 module.exports = NodeHelper.create({
   socketNotificationReceived (notification, payload) {
     switch(notification) {
       case "INIT":
-        this.config = payload;
         this.initialize();
         break;
     }
   },
 
   initialize () {
-    if (this.config.debug) log = (...args) => { console.log("[SCREEN]", ...args); };
     console.log("[SCREEN] EXT-Screen Version:", require("./package.json").version, "rev:", require("./package.json").rev);
     this.sendSocketNotification("INITIALIZED");
   }
